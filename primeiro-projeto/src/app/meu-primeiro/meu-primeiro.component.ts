12cf80f8c01b610fbc8353ccac8549c320c03693
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2022-06-22 10:57:18 -0300

    salvo projeto

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com Angular 14!</p>
+    `
+})
+export class MeuPrimeiroComponent { };
