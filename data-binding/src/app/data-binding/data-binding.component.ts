e3d280148ca88041bd54c59c1745695776be3ac4
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2022-06-24 18:26:38 -0300

    EVent Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,12 +11,27 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com.br/400/200/nature';
 
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botao clicado');
+  }
+
+  onKeyUp(evento:KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  }
+  
+  salvarValor(valor: any){
+    this.valorSalvo = valor;
   }
 
   constructor() { }
